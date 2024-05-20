5c9c72e3f10409b46d122b46c67e46517b2244ff
BrunoDS01 <bdisanzo@itba.edu.ar>
2024-05-19 21:24:23 -0300

    corregido angulo

--- testing_videos.py
+++ testing_videos.py
@@ -4,23 +4,28 @@ import mediapipe as mp
 import numpy as np
 import matplotlib.pyplot as plt
 
-path = 'Videos Bruno\\bruno_3_metros_cortado.mp4' 
+path = 'Videos Bruno\\bruno_6_metros_cort2.mp4' 
 
 # Point1 es el de izquierda 
 # Point 2 es el de derecha
 # El ángulo lo da en sentido horario
 def angles_calc(point1, center, point2):
-     vector1 = [point1[0] - center[0], point1[1] - center[1]]
-     vector2 = [point2[0] - center[0], point2[1] - center[1]]
-     mod_vector1 = np.sqrt(vector1[0] * vector1[0] + vector1[1]*vector1[1] )
-     mod_vector2 = np.sqrt(vector2[0] * vector2[0] + vector2[1]*vector2[1] )
-     cos_angle = (vector1[0] * vector2[0] + vector1[1]*vector2[1])/(  mod_vector1 * mod_vector2 )
-     
-     cos_angle = max(-1, min(1, cos_angle))
+    vector1 = [point1[0] - center[0], point1[1] - center[1]]
+    vector2 = [point2[0] - center[0], point2[1] - center[1]]
+    mod_vector1 = np.sqrt(vector1[0] * vector1[0] + vector1[1]*vector1[1] )
+    mod_vector2 = np.sqrt(vector2[0] * vector2[0] + vector2[1]*vector2[1] )
+    cos_angle = (vector1[0] * vector2[0] + vector1[1]*vector2[1]) / (mod_vector1 * mod_vector2)
 
-     angle = np.arccos(cos_angle)
-     
-     return angle/(np.pi) * 180
+    angle = np.arccos(cos_angle)
+
+    angle_deg = angle * 180 / np.pi
+
+     # Determinar el signo del ángulo usando producto cruzado
+    cross_product = vector1[0] * vector2[1] - vector1[1] * vector2[0]
+    if cross_product < 0:
+        angle_deg = 360 - angle_deg
+
+    return angle_deg
 
 
 mp_drawing = mp.solutions.drawing_utils
@@ -67,15 +72,15 @@ with mp_pose.Pose(static_image_mode = False, smooth_landmarks = True, min_detect
                 
                 if(results.pose_landmarks != None):
                     landmarks = results.pose_landmarks.landmark
-                    right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-                    right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
-                    right_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
-                    right_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
-                    right_foot_index = [landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].y]
+                    right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,-landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                    right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,-landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+                    right_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,-landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+                    right_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,-landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+                    right_foot_index = [landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].x,-landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].y]
                     
                     angle_A.append(angles_calc(right_shoulder, right_hip, right_knee))
                     angle_B.append(angles_calc(right_hip, right_knee, right_ankle))
-                    angle_C.append(angles_calc(right_knee, right_ankle, right_foot_index))
+                    angle_C.append(angles_calc(right_foot_index, right_ankle, right_knee))
                     
                     '''print('Frame:',i)
                     print('Right Shoulder:',right_shoulder)
@@ -85,16 +90,17 @@ with mp_pose.Pose(static_image_mode = False, smooth_landmarks = True, min_detect
                     print('Right Foot:',right_foot_index)'''
                     
                     
-                    '''x = [right_shoulder[0], right_hip[0], right_knee[0], right_ankle[0], right_foot_index[0]]
-                    y = [-right_shoulder[1], -right_hip[1], -right_knee[1], -right_ankle[1], -right_foot_index[1]]
-                    plt.scatter(x, y, color = 'black')
-                    plt.gca().set_aspect(aspect = 9/16, adjustable=None, anchor=None, share=False)
-                    plt.show()'''
+                    '''
                     if (i == 15):
+                        x = [right_shoulder[0], right_hip[0], right_knee[0], right_ankle[0], right_foot_index[0]]
+                        y = [right_shoulder[1], right_hip[1], right_knee[1], right_ankle[1], right_foot_index[1]]
+                        plt.scatter(x, y, color = 'black')
+                        plt.gca().set_aspect(aspect = 9/16, adjustable=None, anchor=None, share=False)
+                        plt.show()
                         while(not (cv2.waitKey(10) & 0xFF == ord('q'))):
                             print(angles_calc(right_shoulder, right_hip, right_knee)) 
                             pass
-                             
+                    '''         
 
                     if cv2.waitKey(10) & 0xFF == ord('q'):
                         break
